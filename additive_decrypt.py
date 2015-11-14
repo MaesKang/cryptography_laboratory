c69dec30feaa5ed07308c0549f0adb001d655209
suman95 <sumansahu95@gmail.com>
2015-11-14 15:22:15 +0000

    Crack Affine

--- additive_decrypt.py
+++ additive_decrypt.py
@@ -1,24 +1,11 @@
-# Suman Sahu
-# additive cipher 
 
-#encryption
-def encrypt(text,key):
-	text = text.toupper()
-	temp = ''
-	for i in range(0,len(text)):
-		temp += chr(((ord(text[i])-ord('A'))+key)%26+ord('A'))
-	return temp
+# Brute force attack on cipher text
 
-#brute-force approach to break additive cipher
-t = 'UVACLYFZLJBYL' # write the givens statement
+t = str(raw_input("Enter the Cipher Text : "))
+t = t.upper()
 l = 0
 for i in range(1,26):
 	temp = ''
 	for j in range(0,len(t)):
 		temp += chr(((ord(t[j])-ord('A')+i)%26+ord('A')))
-	print("Iteration No. {0}".format(i))
-	print(temp)
-
-
-
-
+	print("Iteration No. {0} : {1} ".format(i, temp))
